906ca7e8b854d88314aaf6785c12122aa5dcd109
parksiwoong <tldnd5656@naver.com>
2022-08-21 20:48:35 +0900

    5.특정문자뒤집기(ToCharArray())

---
+++ src/main/java/org/example/ToCharArray_5.java
@@ -0,0 +1,29 @@
+package org.example;
+
+//5. 특정문자 뒤집기
+
+//영어알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고
+//특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램 작성
+
+//입력 : 첫 줄에 기링가 100을 넘지 않는 문자열이 주어짐
+
+//출력: 첫줄에 알파벳만 뒤집힌 문자열을 줄력함
+//예시입력 a#b!GE*T@s   예시출력: S#T!EG*B@a
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ToCharArray_5 {
+
+    //바꾸는 메서드
+    public ArrayList<String> cg (){
+
+        return null;
+    };
+
+    public static void main(String[] args) {
+        ToCharArray_5 T = new ToCharArray_5();
+        Scanner sc = new Scanner(System.in);
+        // sc.nextInt();
+    }
+}
