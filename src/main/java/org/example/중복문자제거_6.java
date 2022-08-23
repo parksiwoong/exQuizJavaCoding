e20f69f527ca5f9e265ccec66ae28807828dece5
parksiwoong <tldnd5656@naver.com>
2022-08-23 23:31:44 +0900

    6.중복문자제거

--- src/main/java/org/example/중복문자제거_6.java
+++ src/main/java/org/example/중복문자제거_6.java
@@ -14,8 +14,13 @@ import java.util.Scanner;
  * */
 public class 중복문자제거_6 {
 
+    /** 중복문자 제거할 메서드 */
     public String solution(String str){
         String answer = "";
+        for(int i=0; i<str.length(); i++){
+           // System.out.println(str[i]+ " " + i + " " + str.indexOf(str.charAt(i)));
+            if(str.indexOf(str.charAt(i))==i) answer += str.charAt(i);
+        }
         return answer;
     }
 
