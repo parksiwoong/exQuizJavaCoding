ba596398221445f48724c00c7bc81ece58de594e
parksiwoong <tldnd5656@naver.com>
2022-08-23 23:34:20 +0900

    6.중복문자제거

--- src/main/java/org/example/중복문자제거_6.java
+++ src/main/java/org/example/중복문자제거_6.java
@@ -19,7 +19,8 @@ public class 중복문자제거_6 {
         String answer = "";
         for(int i=0; i<str.length(); i++){
            // System.out.println(str[i]+ " " + i + " " + str.indexOf(str.charAt(i)));
-            if(str.indexOf(str.charAt(i))==i) answer += str.charAt(i);
+            //charAt으로 처음인 i의 []자리와 그냥 i의 글자 [] 주소 자리와 같을때 보여줌
+            if(str.indexOf(str.charAt(i))==i) answer += str.charAt(i); //charAt가 처음으로 발견될때 쓰는 내부함수
         }
         return answer;
     }
